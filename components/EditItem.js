f961401ba2fbbaf7107a527135b49afb8a61a3e1
Alex <poberezhskiy.a@gmail.com>
2023-01-24 23:53:49 -0800

    finally got the state from edit properly working

--- components/EditItem.js
+++ components/EditItem.js
@@ -1,32 +1,79 @@
-import React from 'react'
-import { useState } from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-export default function EditItem({item}) {
-  const [category, setCategory] = useState(null)
-  const [name, setName] = useState(item.name)
-  const [description, setDescription] = useState(item.description)
-  const [price, setPrice] = useState(item.price)
+export default function EditItem({ item, supabase, updateItemState }) {
+  const [category, setCategory] = useState(item.category);
+  const [name, setName] = useState(item.name);
+  const [description, setDescription] = useState(item.description);
+  const [price, setPrice] = useState(item.price);
+
+  async function updateItem(item, name, description, price) {
+    try {
+      let payload = {
+        category: category,
+        name: name,
+        price: price,
+        description: description,
+      };
+
+      let { data, error } = await supabase
+        .from("item")
+        .update(payload)
+        .eq("id", item.id)
+        .select();
+
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        updateItemState(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  }
+
   return (
-    <Box sx={{display: 'flex', justifyContent: 'space-between', m: 0.5, alignItems: 'center'}}>
-      <Box sx={{display: 'flex', flexDirection: 'column'}}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        m: 0.5,
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
         <TextField
           value={name}
           variant="filled"
           size="small"
+          onChange={(e) => setName(e.target.value)}
         />
         <TextField
           value={description}
           variant="filled"
           size="small"
+          onChange={(e) => setDescription(e.target.value)}
         />
       </Box>
       <TextField
         value={price}
         variant="filled"
         size="small"
+        onChange={(e) => setPrice(e.target.value)}
       />
-  </Box>
-  )
+      <Button
+        onClick={() => {
+          updateItem(item, name, description, price);
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
+  );
 }
--- pages/account/[menu_id].js
+++ pages/account/[menu_id].js
@@ -1,96 +1,141 @@
-import { getClientBuildManifest } from 'next/dist/client/route-loader'
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
-import Divider from '@mui/material/Divider';
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Autocomplete, {createFilterOptions} from '@mui/material/Autocomplete';
-import _ from 'lodash'
-import AddMenuItem from '../../components/AddMenuItem';
-import EditItem from '../../components/EditItem';
+import { getClientBuildManifest } from "next/dist/client/route-loader";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
+import _ from "lodash";
+import AddMenuItem from "../../components/AddMenuItem";
+import EditItem from "../../components/EditItem";
 
 export default function Menu() {
-  const router = useRouter()
-  const supabase = useSupabaseClient()
-  const menuId = router.query.menu_id
-  const [items, setItems] = useState(null)
-  const [category, setCategory] = useState(null)
-  const [name, setName] = useState(null)
-  const [price, setPrice] = useState(null)
-  const [description, setDescription] = useState(null)
-  let grouped = _.groupBy(items, item => item.category)
+  const router = useRouter();
+  const supabase = useSupabaseClient();
+  const menuId = router.query.menu_id;
+  const [items, setItems] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [name, setName] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [description, setDescription] = useState(null);
   useEffect(() => {
-    if (router.isReady) getItems()
-  }, [router.isReady])
+    if (router.isReady) getItems();
+  }, [router.isReady]);
+  let grouped = _.groupBy(items, (item) => item.category);
+  console.log(items);
 
-  async function getItems(){
+  async function getItems() {
     try {
       let { data, error, status } = await supabase
-        .from('item')
+        .from("item")
         .select(`id, category, name, price, description, menu_id`)
-        .eq('menu_id', menuId)
+        .eq("menu_id", menuId);
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setItems(data)
+        setItems(data);
       }
-
     } catch (error) {
-        alert(`${error}`)
+      alert(`${error}`);
     } finally {
-
     }
   }
 
   return (
-    <Paper sx={{minHeight: "95vh", minWidth: '360px', maxWidth: '800px', margin: "auto", background: "#fef9f4", borderRadius: "25px", display: 'flex', flexDirection:"column"}}>
-      <Typography variant='h5' align='center' sx={{pt: 1}}>{router.query.title}</Typography>
-      <Typography variant='subtitle1' align='center' color="#807D7A">{router.query.desc}</Typography>
+    <Paper
+      sx={{
+        minHeight: "95vh",
+        minWidth: "360px",
+        maxWidth: "800px",
+        margin: "auto",
+        background: "#fef9f4",
+        borderRadius: "25px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h5" align="center" sx={{ pt: 1 }}>
+        {router.query.title}
+      </Typography>
+      <Typography variant="subtitle1" align="center" color="#807D7A">
+        {router.query.desc}
+      </Typography>
       <Divider variant="middle" />
 
-      <Box sx={{display: 'flex', flexWrap: 'wrap', width: "100%"}}>
-        {
-          Object.keys(grouped).map(category => {
-            let items = (grouped[`${category}`])
-            return (
-              <Box sx={{display: 'flex', flexWrap: 'wrap', width: '80%',flexDirection: "column", minWidth: '150px', m:"auto"}}>
-                <Typography variant="h6" align='center'>{category}</Typography>
-                {items.map(item => {
-                   
-                  return (
-                    <>
-                      <Box sx={{display: 'flex', justifyContent: 'space-between', m: 0.5, alignItems: 'center'}}>
-                        <Box>
-                          <Typography variant="subtitle1">{item.name}</Typography>
-                          <Typography variant="body1" color="grey">{item.description}</Typography>
-                        </Box>
-                        <Typography variant="button">{item.price}</Typography>
+      <Box sx={{ display: "flex", flexWrap: "wrap", width: "100%" }}>
+        {Object.keys(grouped).map((category) => {
+          let itemsGrouped = grouped[`${category}`];
+          return (
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                width: "80%",
+                flexDirection: "column",
+                minWidth: "150px",
+                m: "auto",
+              }}
+            >
+              <Typography variant="h6" align="center">
+                {category}
+              </Typography>
+              {itemsGrouped.map((item) => {
+                return (
+                  <>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        m: 0.5,
+                        alignItems: "center",
+                      }}
+                    >
+                      <Box>
+                        <Typography variant="subtitle1">{item.name}</Typography>
+                        <Typography variant="body1" color="grey">
+                          {item.description}
+                        </Typography>
                       </Box>
+                      <Typography variant="button">{item.price}</Typography>
+                    </Box>
 
-                      <EditItem item={item}/>
-                      <Divider />
-                    </>
-
-                  )
-                })}
-              </Box>
-            )
-          })
-        }
+                    <EditItem
+                      item={item}
+                      supabase={supabase}
+                      updateItemState={(payload) => {
+                        setItems(
+                          items.map((item) => {
+                            if (item.id === payload[0].id) {
+                              return payload[0];
+                            } else {
+                              return item;
+                            }
+                          })
+                        );
+                      }}
+                    />
+                    <Divider />
+                  </>
+                );
+              })}
+            </Box>
+          );
+        })}
       </Box>
 
       <AddMenuItem
         grouped={grouped}
         menuId={menuId}
         supabase={supabase}
-        addItem={(payload) => setItems([...items, payload])}/>
+        addItem={(payload) => setItems([...items, payload])}
+      />
     </Paper>
-    );
-  }
+  );
+}
