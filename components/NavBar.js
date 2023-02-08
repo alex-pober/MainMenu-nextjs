9c58caf3dbb70caf5375f07ea583d5713f5c9bde
Alex <poberezhskiy.a@gmail.com>
2023-02-07 19:04:21 -0800

    added proper drawer to navbar that it dynamic for mobile viewport

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
@@ -16,72 +15,112 @@ import { Button } from "@mui/material";
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from "next/link";
 import {Link as Muilink} from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Dashboard } from "@mui/icons-material";
+
 export default function NavBar() {
   const session = useSession();
-  const [menuToggle, setMenuToggle] = useState(null);
-  console.log(session);
+  const [drawer, setDrawer] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(theme.palette);
+
+  const drawerContents = () => (
+    <Box
+      sx={{
+        width: 'auto',
+        backgroundColor: 'theme.palette.background.paper'
+      }}
+      role="presentation"
+      onClick={() => setDrawer(!drawer)}
+    >
+      <List>
+        <ListItem key='Dashboard' disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <BallotTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary='Dashboard' />
+            </ListItemButton>
+        </ListItem>
+        <ListItem key='View Live Menu' disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MenuBookTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary='View Live Menu' />
+            </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key='Sign Out' disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LogoutTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary='Sign Out' />
+            </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
+
   return (
     <AppBar position="static" elevation={1} color="transparent">
-      <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
+      <Toolbar variant="regular" sx={{ justifyContent: "space-between" }}>
         <Image
-          width={200}
+          width={100}
           height={100}
           src="https://cpnjwzukiwcoiurbkavl.supabase.co/storage/v1/object/public/assets/main-menu-logo.png"
         />
-        {session && (
-          <>
-          <div>
-            <Link underline="hover" href="/account">Dashboard</Link>
-          </div>
-            <div>
+          {isMobile ? (
+            <>
+              <Drawer
+                anchor="top"
+                open={drawer}
+                onClose={() => setDrawer(!drawer)}
+              >
+              {drawerContents()}
+              </Drawer>
               <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={(e) => {
-                  setMenuToggle(e.currentTarget);
-                }}
-                color="inherit"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={() => setDrawer(!drawer)}
+              edge="start"
+              // sx={{ mr: 2, ...(open && { display: 'none' }) }}
               >
-                <AccountCircle />
+                <MenuIcon />
               </IconButton>
-
-              <Menu
-                id="menu-appbar"
-                anchorEl={menuToggle}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(menuToggle)}
-                onClose={() => {
-                  setMenuToggle(null);
-                }}
-              >
+            </>
+          ) : (
+            <>
+              <Box sx={{display: 'flex'}}>
                 <MenuItem>
                   <BallotTwoToneIcon />
-                  Dashboard
+                    Dashboard
                 </MenuItem>
-
                 <MenuItem>
                   <MenuBookTwoToneIcon />
                   View Live Menu
                 </MenuItem>
-
+              </Box>
+              <Box sx={{display: 'flex'}}>
                 <MenuItem>
                   <LogoutTwoToneIcon />
                   Sign Out
                 </MenuItem>
-              </Menu>
-            </div>
-          </>
-        )}
+              </Box>
+            </>
+          )}
       </Toolbar>
     </AppBar>
   );
