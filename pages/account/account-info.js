748a265c15cf4eba861dc18a00902e178c2530b9
Alex <poberezhskiy.a@gmail.com>
2023-02-22 22:00:28 -0800

    progress on seperating componenets into seperate routes

--- pages/account/account-info.js
+++ pages/account/account-info.js
@@ -1,13 +1,49 @@
 import { useState, useEffect } from "react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import ManageMenus from "../../components/ManageMenus";
+import Link from "next/link";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import NavBar from "../../components/NavBar";
+import Tabs from "@mui/material/Tabs";
 
-export default function AccountInfo({ session, userData }) {
+export default function AccountInfo() {
   const supabase = useSupabaseClient();
   const user = useUser();
+  const [userData, setUserData] = useState({});
   const [username, setUsername] = useState(userData?.username);
   const [contactEmail, setContactEmail] = useState(userData?.contact_email);
   const [contactNumber, setContactNumber] = useState(userData?.contact_number);
   const [loading, setLoading] = useState(false);
+  const [tab, setTab] = useState(1);
+  console.log(tab)
+  async function getProfile(userId) {
+    try {
+      // setLoading(true);
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, contact_email, contact_number`)
+        .eq("id", userId)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUserData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // setLoading(false);
+    }
+  }
 
   async function updateProfile({ username, contactEmail, contactNumber }) {
     try {
@@ -31,11 +67,47 @@ export default function AccountInfo({ session, userData }) {
       setLoading(false);
     }
   }
+  useEffect(() => {
+    async function loadData() {
+      getProfile(user.id);
+    }
+
+    if (user) loadData();
+  }, [user]);
+
   return (
     <>
+      <NavBar />
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          centered
+          color="default"
+          sx={{ backgroundColor: "#f5f5f5" }}
+          value={tab}
+          onChange={(event, newValue) => {
+            setTab(newValue);
+          }}
+        >
+          <Tab
+            component="a"
+            label="Manage Menus"
+            href="/menu-manager"
+          />
+          <Tab
+            component="a"
+            label="Account Info"
+            href="account/account-info"
+          />
+        </Tabs>
+      </Box>
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
+        <input
+          id="email"
+          type="text"
+          value={userData.contact_email || ""}
+          disabled
+        />
       </div>
       <div>
         <label htmlFor="username">Username</label>
